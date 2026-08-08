87ef53fdb73a27c0a2570161ebb803fdd3d1bb26
L1ZeZzz <3684242047@qq.com>
2026-08-08 13:16:54 +0800

    Update RenderMixin.java

--- 1.8.9/cloudclient/src/main/java/dev/cloudmc/mixins/RenderMixin.java
+++ 1.8.9/cloudclient/src/main/java/dev/cloudmc/mixins/RenderMixin.java
@@ -2,7 +2,7 @@ package dev.cloudmc.mixins;
 
 import dev.cloudmc.Cloud;
 import dev.cloudmc.feature.mod.impl.FreelookMod;
-import dev.cloudmc.helpers.render.GLHelper;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
@@ -11,6 +11,7 @@ import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -27,6 +28,10 @@ public abstract class RenderMixin {
     public FontRenderer getFontRendererFromRenderManager() {
         return null;
     }
+
+    // 🔥 你的图标资源路径（改成你的实际图片名）
+    private static final ResourceLocation ICON_TEXTURE = 
+        new ResourceLocation("cloudmc", "icon/nametag_icon.png");
 
     /**
      * @author duplicat
@@ -49,8 +54,8 @@ public abstract class RenderMixin {
             GlStateManager.pushMatrix();
             GlStateManager.translate((float) x + 0.0F, (float) y + entityIn.height + 0.5F + yPos, (float) z);
             GL11.glNormal3f(0.0F, 1.0F, 0.0F);
-            
-            // 🔥 关键修改：FreeLook 激活时使用 FreeLook 的摄像机角度
+
+            // 🔥 FreeLook 激活时使用 FreeLook 的摄像机角度
             if (FreelookMod.cameraToggled) {
                 GlStateManager.rotate(-FreelookMod.cameraYaw, 0.0F, 1.0F, 0.0F);
                 GlStateManager.rotate(FreelookMod.cameraPitch, 1.0F, 0.0F, 0.0F);
@@ -58,7 +63,7 @@ public abstract class RenderMixin {
                 GlStateManager.rotate(-this.renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
                 GlStateManager.rotate(this.renderManager.playerViewX, 1.0F, 0.0F, 0.0F);
             }
-            
+
             GlStateManager.scale(-f1 * scale, -f1 * scale, f1);
             GlStateManager.disableLighting();
             GlStateManager.depthMask(false);
@@ -73,6 +78,32 @@ public abstract class RenderMixin {
             }
 
             int j = fontrenderer.getStringWidth(str) / 2;
+
+            // 🔥 绘制头顶图标（在名字左侧）
+            if (nameTagToggled) {
+                GlStateManager.pushMatrix();
+                GlStateManager.scale(0.5F, 0.5F, 1.0F);
+
+                Minecraft.getMinecraft().getTextureManager().bindTexture(ICON_TEXTURE);
+
+                int iconSize = 16;
+                int xPos = (-j - iconSize - 4) * 2;
+                int yPosIcon = (i - 2) * 2;
+
+                GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+                Tessellator tessellator = Tessellator.getInstance();
+                WorldRenderer worldrenderer = tessellator.getWorldRenderer();
+                worldrenderer.begin(7, DefaultVertexFormats.POSITION_TEX);
+                worldrenderer.pos(xPos, yPosIcon + iconSize, 0.0D).tex(0, 1).endVertex();
+                worldrenderer.pos(xPos + iconSize, yPosIcon + iconSize, 0.0D).tex(1, 1).endVertex();
+                worldrenderer.pos(xPos + iconSize, yPosIcon, 0.0D).tex(1, 0).endVertex();
+                worldrenderer.pos(xPos, yPosIcon, 0.0D).tex(0, 0).endVertex();
+                tessellator.draw();
+
+                GlStateManager.popMatrix();
+            }
+
+            // 渲染名字背景阴影
             Tessellator tessellator = Tessellator.getInstance();
             WorldRenderer worldrenderer = tessellator.getWorldRenderer();
             GlStateManager.disableTexture2D();
@@ -84,6 +115,7 @@ public abstract class RenderMixin {
             tessellator.draw();
             GlStateManager.enableTexture2D();
 
+            // 渲染名字
             fontrenderer.drawString(str, -fontrenderer.getStringWidth(str) / 2, i, nameTagToggled ? color : 553648127);
             GlStateManager.enableDepth();
             GlStateManager.depthMask(true);
